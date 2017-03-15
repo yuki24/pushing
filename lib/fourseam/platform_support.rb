f521b55306668288bf486deb1783e8e19c7bb37f
Yuki Nishijima <mail@yukinishijima.net>
2017-03-15 02:11:11 +0000

    ✂️ warning: class variable @@apn of Fourseam::Base is overtaken by Fourseam::PlatformSupport

--- lib/fourseam/platform_support.rb
+++ lib/fourseam/platform_support.rb
@@ -10,13 +10,13 @@ module Fourseam
       # self.platforms = [:apn, :fcm]
       self.platforms = [:fcm]
 
-      cattr_reader :apn
-      @@apn = PlatformSupport::Apn.new
-      apn.adapter = :houston # TODO: Move this to Railties
+      cattr_accessor :apn
+      self.apn = PlatformSupport::Apn.new
+      self.apn.adapter = :houston # TODO: Move this to Railties
 
-      cattr_reader :fcm
-      @@fcm = PlatformSupport::Fcm.new
-      fcm.adapter = :robo_msg # TODO: Move this to Railties
+      cattr_accessor :fcm
+      self.fcm = PlatformSupport::Fcm.new
+      self.fcm.adapter = :robo_msg # TODO: Move this to Railties
     end
   end
 end
