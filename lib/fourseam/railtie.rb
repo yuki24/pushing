b2720e5c2e61fa7552259046e8ff3ad1ceed5f19
Yuki Nishijima <yk.nishijima@gmail.com>
2017-05-16 09:14:00 -0400

    Set up logger

--- lib/fourseam/railtie.rb
+++ lib/fourseam/railtie.rb
@@ -1,5 +1,11 @@
 module Fourseam
   class Railtie < Rails::Railtie # :nodoc:
+    # config.eager_load_namespaces << Fourseam
+
+    initializer "fourseam.logger" do
+      ActiveSupport.on_load(:fourseam) { self.logger ||= Rails.logger }
+    end
+
     initializer "fourseam.add_view_paths" do |app|
       views = app.config.paths["app/views"].existent
       if !views.empty?
