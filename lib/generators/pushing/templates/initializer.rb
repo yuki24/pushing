0d9a430eb126b3ea094633cdd590d334f6b7f918
Yuki Nishijima <yk.nishijima@gmail.com>
2017-06-03 21:16:17 -0400

    Update initializer template [ci skip]

--- lib/generators/pushing/templates/initializer.rb
+++ lib/generators/pushing/templates/initializer.rb
@@ -3,8 +3,8 @@ Pushing::Platforms.configure do |config|
   config.fcm.server_key = 'YOUR_FCM_TEST_SERVER_KEY'
 
   config.apn.environment          = Rails.env.production? ? :production : :development
-  config.apn.adapter              = Rails.env.test? ? :test : :andpush
-  config.apn.topic                = 'net.yukinishijima.yourapp'
-  config.apn.certificate_path     = '/path/to/your_certificate.pem'
+  config.apn.adapter              = Rails.env.test? ? :test : :apnotic
+  config.apn.topic                = 'com.awesomecompany.app'
+  config.apn.certificate_path     = '/config/your_certificate.pem'
   config.apn.certificate_password = 'PASSWORD_FOR_CERT'
 end
