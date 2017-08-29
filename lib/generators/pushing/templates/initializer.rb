e940d86b2e0dc82571a8367672299c25419e17bb
Yuki Nishijima <yk.nishijima@gmail.com>
2017-08-28 23:55:21 -0400

    Update initilalizer template

--- lib/generators/pushing/templates/initializer.rb
+++ lib/generators/pushing/templates/initializer.rb
@@ -1,10 +1,27 @@
 Pushing::Platforms.configure do |config|
-  config.fcm.adapter    = Rails.env.test? ? :test : :andpush
-  config.fcm.server_key = 'YOUR_FCM_TEST_SERVER_KEY'
+  # Adapter you want to use to send push notifications through FCM
+  config.fcm.adapter = Rails.env.test? ? :test : :andpush
 
-  config.apn.environment          = Rails.env.production? ? :production : :development
-  config.apn.adapter              = Rails.env.test? ? :test : :apnotic
-  config.apn.topic                = 'com.awesomecompany.app'
-  config.apn.certificate_path     = '/config/your_certificate.pem'
-  config.apn.certificate_password = 'PASSWORD_FOR_CERT'
+  # Your FCM servery key that can be found here: https://console.firebase.google.com/project/_/settings/cloudmessaging
+  config.fcm.server_key = 'YOUR_FCM_SERVER_KEY'
+
+  # Adapter you want to use to send push notifications through APNs
+  config.apn.adapter = Rails.env.test? ? :test : :apnotic
+
+  # The environment that is used by default to send push notifications through APNs
+  config.apn.environment = Rails.env.production? ? :production : :development
+
+  # Path to the ecrtificate for establishing a connection to APNs
+  config.apn.certificate_path = 'path/to/your/certificate'
+
+  # Password for the certificate specified above if there's any
+  config.apn.certificate_password = 'passphrase'
+
+  # Header values that are added to every request to APNs. documentation for this
+  # can be found here: https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/CommunicatingwithAPNs.html#//apple_ref/doc/uid/TP40008194-CH11-SW13
+  config.apn.default_headers = {
+    apns_priority:    10,
+    apns_topic:       'your.awesomeapp.ios',
+    apns_collapse_id: 'wrong.topicname.com'
+  }
 end
