b4d1562473237c4afef6251d519dd0ddd0f3d826
Yuki Nishijima <yk.nishijima@gmail.com>
2018-11-11 16:35:55 -0500

    ✂️ '`Pushing::Platforms.config' is deprecated and will be removed in 0.4.0. Please use `Pushing.config' instead'

--- test/integration/adapters/lowdown_test.rb
+++ test/integration/adapters/lowdown_test.rb
@@ -5,9 +5,9 @@ class LowdownTest < ActiveSupport::TestCase
   include ApnHttp2TestCases
 
   setup do
-    Pushing::Platforms.config.apn.adapter = :lowdown
-    Pushing::Platforms.config.apn.connection_scheme = :certificate
-    Pushing::Platforms.config.apn.certificate_path = File.join(File.expand_path("./"), ENV.fetch('APN_TEST_CERTIFICATE_PATH'))
+    Pushing.config.apn.adapter = :lowdown
+    Pushing.config.apn.connection_scheme = :certificate
+    Pushing.config.apn.certificate_path = File.join(File.expand_path("./"), ENV.fetch('APN_TEST_CERTIFICATE_PATH'))
   end
 
   private
